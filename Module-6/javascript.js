3287acb43b18f77b2190d858ec06815a29c5e2c6
Vanessa Corlette <nessacor16@gmail.com>
2019-10-18 21:21:55 -0400

    Prework Commit

---
+++ Module-6/javascript.js
@@ -0,0 +1,18 @@
+/* javascript */
+document.getElementById("button1").addEventListener("click", function()
+{
+    document.getElementById("box").style.height = "300px";
+
+});
+document.getElementById("button2").addEventListener("click", function()
+{
+    document.getElementById("box").style.background = "blue";
+});
+document.getElementById("button3").addEventListener("click", function()
+{
+    document.getElementById("box").style.opacity= "0.3";
+});
+document.getElementById("button4").addEventListener("click", function()
+{
+    document.getElementById("box").style.resetdocument= "Reset";
+});
